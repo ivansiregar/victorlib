08c728212079d022febe37ec164d9092e0c0a492
ivan siregar <ivan.siregar@gmail.com>
2021-05-22 12:46:44 +0700

    second commit on 2021-05-22

--- victorlib/src/main/java/com/example/victorlib/Calculate.kt
+++ victorlib/src/main/java/com/example/victorlib/Calculate.kt
@@ -12,5 +12,11 @@ class Calculate
         fun kurang(i1: Int, i2: Int): Int {
             return (i1 - i2)
         }
+
+        fun kali(i1: Int, i2: Int): Int {
+            return (i1 * i2)
+        }
+
+
     }
 }
