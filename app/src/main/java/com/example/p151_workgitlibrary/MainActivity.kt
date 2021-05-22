81d608c4737a62c3df1dec0f703af3fb10b14f60
ivan siregar <ivan.siregar@gmail.com>
2021-05-22 12:32:20 +0700

    first commit

---
+++ app/src/main/java/com/example/p151_workgitlibrary/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.p151_workgitlibrary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ victorlib/src/main/java/com/example/victorlib/Calculate.kt
@@ -0,0 +1,16 @@
+package com.example.victorlib
+
+class Calculate
+{
+    companion object {
+
+        fun tambah(i1: Int, i2: Int): Int {
+            return (i1 + i2)
+        }
+
+
+        fun kurang(i1: Int, i2: Int): Int {
+            return (i1 - i2)
+        }
+    }
+}
